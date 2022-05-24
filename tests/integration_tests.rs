76d3c4bae165e1a529d5b1cac90cb99ca5ba3d05
Braden Adams <braden.adams@sap.com>
2022-05-24 15:41:55 +0100

    Adding support for custom number formats

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,5 +1,5 @@
 extern crate simple_excel_writer;
-
+use std::io::{Cursor, Read};
 use excel::*;
 use simple_excel_writer as excel;
 
@@ -63,6 +63,14 @@ fn creates_and_saves_an_excel_sheet_driver(filename: Option<&str>) -> Option<Vec
     wb.close().expect("Close excel error!")
 }
 
+fn get_style_from_zip(mem_file: Vec<u8>) -> String {
+    let mut archive = zip::read::ZipArchive::new(Cursor::new(mem_file)).unwrap();
+    let mut styles_file = archive.by_name("xl/styles.xml").unwrap();
+    let mut temp_buf = vec![];
+    let _ = styles_file.read_to_end(&mut temp_buf).unwrap();
+    std::str::from_utf8(&temp_buf[..]).unwrap().to_string()
+}
+
 #[test]
 fn creates_and_saves_an_excel_sheet() {
     let file_test = creates_and_saves_an_excel_sheet_driver(Some("test.xlsx"));
@@ -70,4 +78,116 @@ fn creates_and_saves_an_excel_sheet() {
 
     let in_memory_test = creates_and_saves_an_excel_sheet_driver(None);
     assert!(in_memory_test.is_some());
+}
+
+#[test]
+fn creates_file_and_checks_default_style() {
+    let expected_xml = r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main"
+            xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">
+    <fonts count="1">
+        <font>
+            <sz val="12"/>
+            <color theme="1"/>
+            <name val="Calibri"/>
+            <family val="2"/>
+            <scheme val="minor"/>
+        </font>
+    </fonts>
+    <fills count="2">
+        <fill>
+            <patternFill patternType="none"/>
+        </fill>
+        <fill>
+            <patternFill patternType="gray125"/>
+        </fill>
+    </fills>
+    <borders count="1">
+        <border>
+            <left/>
+            <right/>
+            <top/>
+            <bottom/>
+            <diagonal/>
+        </border>
+    </borders>
+    <cellStyleXfs count="1">
+        <xf numFmtId="0" fontId="0" fillId="0" borderId="0"/>
+    </cellStyleXfs>
+    <cellXfs count="3">
+        <xf numFmtId="0" fontId="0" fillId="0" borderId="0" xfId="0" applyNumberFormat="1"/>
+        <xf numFmtId="14" fontId="0" fillId="0" borderId="0" xfId="0" applyNumberFormat="1"/>
+        <xf numFmtId="22" fontId="0" fillId="0" borderId="0" xfId="0" applyNumberFormat="1"/>
+    </cellXfs>
+    <cellStyles count="1">
+        <cellStyle name="Normal" xfId="0" builtinId="0"/>
+    </cellStyles>
+    <dxfs count="0"/>
+    <tableStyles count="0" defaultTableStyle="TableStyleMedium9" defaultPivotStyle="PivotStyleMedium4"/>
+</styleSheet>"#.to_string();
+    let mem_file = creates_and_saves_an_excel_sheet_driver(None).unwrap();
+    let result = get_style_from_zip(mem_file);
+    assert_eq!(expected_xml, result, "The style sheet should match!");
+}
+
+#[test]
+fn creates_file_with_custom_number_format_and_checks_style() {
+    let expected_xml = r##"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main"
+            xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">
+    <numFmts count="3">
+        <numFmt numFmtId="165" formatCode="&quot;$&quot;#,##0.00"/>
+        <numFmt numFmtId="166" formatCode="#,##0.00&quot; KG&quot;"/>
+        <numFmt numFmtId="167" formatCode="#,##0.0&quot;&lt;&gt;&quot;"/>
+    </numFmts>
+    <fonts count="1">
+        <font>
+            <sz val="12"/>
+            <color theme="1"/>
+            <name val="Calibri"/>
+            <family val="2"/>
+            <scheme val="minor"/>
+        </font>
+    </fonts>
+    <fills count="2">
+        <fill>
+            <patternFill patternType="none"/>
+        </fill>
+        <fill>
+            <patternFill patternType="gray125"/>
+        </fill>
+    </fills>
+    <borders count="1">
+        <border>
+            <left/>
+            <right/>
+            <top/>
+            <bottom/>
+            <diagonal/>
+        </border>
+    </borders>
+    <cellStyleXfs count="1">
+        <xf numFmtId="0" fontId="0" fillId="0" borderId="0"/>
+    </cellStyleXfs>
+    <cellXfs count="6">
+        <xf numFmtId="0" fontId="0" fillId="0" borderId="0" xfId="0" applyNumberFormat="1"/>
+        <xf numFmtId="14" fontId="0" fillId="0" borderId="0" xfId="0" applyNumberFormat="1"/>
+        <xf numFmtId="22" fontId="0" fillId="0" borderId="0" xfId="0" applyNumberFormat="1"/>
+        <xf numFmtId="165" fontId="0" fillId="0" borderId="0" xfId="0" applyNumberFormat="1"/>
+        <xf numFmtId="166" fontId="0" fillId="0" borderId="0" xfId="0" applyNumberFormat="1"/>
+        <xf numFmtId="167" fontId="0" fillId="0" borderId="0" xfId="0" applyNumberFormat="1"/>
+    </cellXfs>
+    <cellStyles count="1">
+        <cellStyle name="Normal" xfId="0" builtinId="0"/>
+    </cellStyles>
+    <dxfs count="0"/>
+    <tableStyles count="0" defaultTableStyle="TableStyleMedium9" defaultPivotStyle="PivotStyleMedium4"/>
+</styleSheet>"##.to_string();
+    let mut wb = excel::Workbook::create_in_memory();
+    wb.add_number_format("\"$\"#,##0.00".to_string());
+    wb.add_number_format("#,##0.00\" KG\"".to_string());
+    wb.add_number_format("#,##0.0\"<>\"".to_string());
+    let mem_file = wb.close().unwrap().unwrap();
+    let result = get_style_from_zip(mem_file);
+    assert_eq!(expected_xml, result, "The style sheet should match!");
 }
